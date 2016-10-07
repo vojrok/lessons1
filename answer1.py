c94cee07eab67ba25d4815138187216f4b996e3c
vojrok <kd@itcanfly.cz>
2016-10-07 23:28:52 +0400

    Delete answer1.py

--- answer1.py
+++
@@ -1,5 +0,0 @@
-dialog = {"привет": "И тебе привет!", "как дела": "Лучше всех", "пока": "Увидимся"}
-def get_answer(question, answer):
-	return answers.get(question)
-
-print(get_answer('пока', dialog))
